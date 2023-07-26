895ae252bef04225ad010ea2cfd5aa218146aad8
Bruna <brunasgreff@gmail.com>
2023-07-26 17:17:26 -0300

    added ex9

---
+++ EX9-PythonBasico.py
@@ -0,0 +1,11 @@
+# TIPOS SEQUENCIAIS
+
+# Strings - métodos de tratamento
+# *Upper,  Lower e Split 
+
+curso = 'Ensino a Distância'
+print(curso.upper())
+print(curso.lower())
+print(curso.split())
+
+
