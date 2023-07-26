5311feef4798b9ce795c04bff66fe0d0211a36f4
Bruna <brunasgreff@gmail.com>
2023-07-26 17:25:45 -0300

    added ex10

---
+++ EX10-PythonBasico.py
@@ -0,0 +1,13 @@
+# Converssão de tipos de dados
+
+# Valores booleanos - conversão implícita
+x = True + 3 
+print(x)
+y = False + 1
+print(y)
+
+# Conversões explícitas - forçar que o valor seja tratado como se deseja
+x = float(2)
+print(x)
+y = int(5.1)
+print(y)
