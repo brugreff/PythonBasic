054e6e95efa0ef7e0b8dd769618c723e57b53a53
Bruna <brunasgreff@gmail.com>
2023-07-26 11:15:13 -0300

    added ex2

---
+++ EX2-PythonBasico.py
@@ -0,0 +1,7 @@
+# String em Python
+nome = 'exemplo de string em Python'
+print(nome)
+print('valor da variável nome = {}'.format(nome))
+# Tanto faz aspas simples ou dupla
+print(f'valor da variável nome = {nome}')
+print(f"valor da variável nome = {nome}")
