015c1a698ac64681ed674147fb7b11c776cb912f
Bruna <brunasgreff@gmail.com>
2023-07-26 18:08:07 -0300

    added ex13

---
+++ EX13-PythonBasico.py
@@ -0,0 +1,8 @@
+# Obtendo a raiz da equação de primeiro grau
+# encontrar o zero da função f(x)=ax+b -> ax+b=0 -> x=-b/a 
+# a dif. de 0
+
+a = 10
+b = 5
+x = -b/a
+print('A raiz da equação do primeiro grau é: x = {}'.format(x))
