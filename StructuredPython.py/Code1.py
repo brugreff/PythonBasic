e68140005af159d58800726c36d1cec2124d1b45
Bruna <brunasgreff@gmail.com>
2023-07-27 17:07:32 -0300

    added code one

---
+++ StructuredPython.py/Code1.py
@@ -0,0 +1,19 @@
+# Solução que receba dois nº e identifique qual o maior.
+
+# Estratégia 01
+a = 10
+b = 20
+if(a>b):
+    maior = a
+else:
+    maior = b
+print(f'O maio número é: {maior}')
+
+# Estratégia 02
+a = 10
+b = 20
+maior = a
+if(b>maior):
+    maior = b
+print(f'O maior número é: {maior}')
+
