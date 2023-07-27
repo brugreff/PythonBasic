f20651f85b1ae01115b7ee161733052015c81776
Bruna <brunasgreff@gmail.com>
2023-07-27 16:42:41 -0300

    added some sequences examples

---
+++ Sequences.py
@@ -0,0 +1,12 @@
+seq = [0, 1, 2]
+print(seq)
+
+# Imprimindo uma substring
+# - colchetes indicando o intervalo de índices
+str = 'Hello World'
+print(str[0:4]) #  índices 0, 1, 2 e 3 
+print(str[2:8])
+
+# imprimindo a string lida da direita para a esquerda 
+print(str[::-1])
+print(str[8:2:-1])
