d51daf4adbda1d08cedc611f2413abf7be378deb
Bruna <brunasgreff@gmail.com>
2023-07-27 16:25:40 -0300

    added data output format (ex: hours)

---
+++ OutputDataFormatting.py
@@ -0,0 +1,9 @@
+# Determinando padrão para a saída
+hora = 20
+minutos = 15
+segundos = 13
+
+print (str(hora) + ' : ' + str(minutos) + ' : ' + str(segundos))
+
+print(f'{hora} : {minutos} : {segundos}')
+
