05454e9ef0b272995620a3f37ab35b7cf9b8b12a
Bruna <brunasgreff@gmail.com>
2023-07-28 17:26:52 -0300

    infinite loop while with a break

---
+++ StructuredPython/Code10.py
@@ -0,0 +1,7 @@
+# Laço while infinito
+# Instrução aux - break: interrompe repetições
+while True:
+    palavra = input('Entre com uma palavra: \n')
+    if palavra == 'sair':
+        break
+print('Você digitou sair e agora está fora do laço')
