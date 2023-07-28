dad6733395d840e8de01e406620ba8620012d878
Bruna <brunasgreff@gmail.com>
2023-07-28 16:53:25 -0300

    for with a string

---
+++ StructuredPython/Code7.py
@@ -0,0 +1,5 @@
+# soletrar o nome digitado pelo usuário
+
+nome = input("Entre com seu nome: \n") # armazena na variável nome
+for letra in nome: # criação do laço 
+    print(letra) # instrução a ser executada a cada repetição
