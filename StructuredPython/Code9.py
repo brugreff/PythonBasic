424b593a74ebcc71640c7d59d2f86e5e6eabb96d
Bruna <brunasgreff@gmail.com>
2023-07-28 17:16:25 -0300

    loop while

---
+++ StructuredPython/Code9.py
@@ -0,0 +1,6 @@
+# while - usuário deve digitar sair p enerrar o laço
+
+palavra = input('Entre com uma  palavra: \n')
+while palavra != 'sair': # enquanto for diferente de 'sair'
+    palavra  = input('Digite sair para encerrar o laço: \n')
+print('Você digitou sair e agora está fora do laço.')    
