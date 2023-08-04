7758cc4eeedef9fa7db97c14fa0f946c43c136a4
Bruna <brunasgreff@gmail.com>
2023-08-04 19:46:11 -0300

    recursive functions - stop condition

--- StructuredPython/Code22.py
+++ StructuredPython/Code22.py
@@ -1,2 +1,20 @@
-y = 1
-eval(y)
+# Recursividade com condição de parada
+
+def regressiva(x):
+    if x <= 0:           # condição de parada
+        print('Acabou')
+    else:
+        print(x)
+        regressiva(x-1) 
+
+while True:
+    x = input('Digite um número para x: ')
+    try:                                     # teste
+        x = int(x)                           
+        break
+    except:
+        print("Você digitou algo não numérico, digite novamente")
+
+regressiva(x)
+
+# só vai parar a execução quando o input for numérico
