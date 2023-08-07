131801b878b908fee2ced5b4cbb5b324e24244f8
Bruna <brunasgreff@gmail.com>
2023-08-07 19:21:42 -0300

    tkinter module - first window

---
+++ StructuredPython/Code28.py
@@ -0,0 +1,14 @@
+# MÓDULO TKINTER
+
+#Pacote tkinter - interface padrão p/ o Tk GUI toolkit.
+#Biblioteca tkinter permite creiação de janelas c/ elem. gráficos(entrada de dados e botões, por ex)
+
+# Exemplo
+# Criação de janela com alguns elementos
+
+from tkinter import * #importação de todos elementos disp. em tkinter
+
+janelaPrincipal = Tk() # objeto janelaPrincipal é do tipo Tk
+janelaPrincipal.mainloop() # elemento mainloop() faz a janela aparecer
+
+# Um objeto Tk é um elemento que representa a janela GUI (interface gráfica com o usuário)
