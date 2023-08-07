ddeef6a173c3e15d838a998ed37d73fd7ab6b132
Bruna <brunasgreff@gmail.com>
2023-08-07 17:24:42 -0300

    imported module function - math example

---
+++ StructuredPython/Code26.py
@@ -0,0 +1,12 @@
+# Função de módulo importado
+# Passos necessários:
+# 1. Importar módulo desejado com a instrução: import nome_modulo
+# 2. Chamar a função desejada, seguida do nome do módulo, c/ a instrução: nome_modulo.nome_funcao(parâmetros)
+
+# Exemplo
+# Import. módulo math(op matemáticas), calc.raiz quadr. de 5 c/ função sqrt()
+
+import math # Passo 1
+
+x = math.sqrt(5) # Passo 2
+print(x)
