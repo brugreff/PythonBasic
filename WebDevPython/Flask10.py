28c63b2ff7de0a185ee3c5a583db30e78944528a
Bruna <brunasgreff@gmail.com>
2023-08-11 20:18:29 -0300

    flask - html without template 2

---
+++ WebDevPython/Flask10.py
@@ -0,0 +1,24 @@
+# a. Exibir mensagem "Olá, programadores!" no endereço raiz de uma pág web e apareça a mensagem "Entre com dois números"
+# b. Exiba a mensagem:"0.0" no endereço "/somar/"
+# c. Exiba a mensagem: "30.0" no endereço "/somar/10/20" de uma pág web 
+
+# Passagem de parâmetros p/ aplicação através da página 
+# Esperar que ela faça alguma execução
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def cumprimento():
+    boas_vindas = '<h1>Olá, programadores!</h1>'
+    instr = '<p>Entre com dois números</p>'
+    return boas_vindas + instr 
+
+@app.route('/somar/')
+@app.route('/somar/<num01>/<num02>')
+def soma (num01=0, num02=0):
+    resultado = float(num01) + float(num02)
+    return str(resultado)
+
+if __name__ == '__main__':
+    app.run(debug=True)
