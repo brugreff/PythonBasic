cc74e0f4aff5626e3979fc114a2d78b70a433386
Bruna <brunasgreff@gmail.com>
2023-08-14 17:35:59 -0300

    foo

--- DataSciencePython/Code1.py
+++ DataSciencePython/Code1.py
@@ -8,16 +8,21 @@ from sklearn.linear_model import LinearRegression
 import pandas as pd
 
 #### Pré-processamento ####
-# Coleta e integração 
+# Coleta e integração (carregar a planilha dentro do programa)
 arquivo = pd.read_csv('DataSciencePython/dados_dengue.csv')
 
 anos = arquivo[['ano']]
 casos = arquivo[['casos']]
 
 #### Mineração ####
+# Criar objeto do tipo LinearRegression e atribuí-lo a uma variável
+# Objeto irá treinar a equação reta gerada pela regressão
+# Fit() - precisa dos parâmetros X e y p/ realizar o treinamento.
 regr = LinearRegression()
-regr.fit(X = anos, y = casos)
+regr.fit(X = anos, y = casos) # X: dados do treinamento/ y: respectivos resultados
 
+# Objeton regr está pronto p/ ser utilizado p/ predizer os casos p/ anos futuros
+# Utilização do métoto predict()
 ano_futuro = [[2018]]
 casos_2018 = regr.predict(ano_futuro)
 
