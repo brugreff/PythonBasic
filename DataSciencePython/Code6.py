641d5eab3e3632ab00f1d760425964da8ba359cb
Bruna <brunasgreff@gmail.com>
2023-08-14 23:35:36 -0300

    load digits example

---
+++ DataSciencePython/Code6.py
@@ -0,0 +1,11 @@
+# Carregar dados da base load_digits
+# Informar a quantidade de dados
+
+from sklearn.datasets import load_digits
+digitos = load_digits()
+
+# Existem 1797 imagens, sendo que cada uma tem uma dimensão 8 x 8 = 64
+print("Shape dos dados de imagens:{}".format(digitos.data.shape))
+# Apresentar o total de dados rotulados com inteiros de 0 a 9
+print("Shape dos dados rotulados: {}".format(digitos.target.shape))
+
